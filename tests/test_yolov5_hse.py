809c7933c0bd480ffb6c4e6f7b6c77f38fff5c03
Berezkin AA <berezkinalexey@yandex.ru>
2022-05-26 18:10:31 +0300

    added executable and model weights to the package

---
+++ tests/test_yolov5_hse.py
@@ -0,0 +1,5 @@
+from yolov5_hse import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
--- yolov5_hse/tests/test_yolov5_hse.py
+++
@@ -1,5 +0,0 @@
-from yolov5_hse import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
